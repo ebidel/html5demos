d694866293745c71c2fa5145a9ab19c86dac511d
Eric Bidelman <ebidel@gmail.com>
2011-03-10 18:25:46 -0800

    Adding bgnews sample

---
+++ html5-demos.appspot.com/bgnews/js/app.js
@@ -0,0 +1,232 @@
+/*Copyright 2011 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author: Eric Bidelman (ericbidelman@chromium.org)
+*/
+const BG_WIN_URL = 'background.html#0';
+const BG_WIN_NAME = 'bgNotifier';
+
+function initSettingsPanel() {
+  /*var feedNames = [];
+  var feeds = document.querySelectorAll('.feed');
+
+  for (var i = 0, feed; feed = feeds[i]; ++i) {
+    feedNames.push(feed.dataset('table'));
+  }*/
+
+  var html = [];
+  for (var table in news.FEEDS) {
+    var id = 'feed-name-' + table;
+    html.push('<li><input type="checkbox" id="', id,
+              '" checked disabled onchange="toggleFeedVisibility(\'', id,
+              '\')"> <label for="feed-name-', table, '">',
+              news.FEEDS[table].replace('http://', ''), '</label></li>');
+  }
+  document.querySelector('#feed-list').innerHTML =  '<ul>' + html.join('') + '</ul>';
+
+  var refreshRate = window.localStorage.getItem('refreshRate');
+  if (!refreshRate) {
+    refreshRate = 30;
+    window.localStorage.setItem('refreshRate', refreshRate);
+  }
+  document.querySelector('#refresh-rate').value =  window.localStorage.getItem('refreshRate');
+  document.querySelector('#num-feeds').textContent = news.NUM_FEEDS;
+}
+
+function onArticleOpen(el) {
+  var isShowing = el.classList.contains('on');
+
+  // Hide article if one is showing.
+  var active = document.querySelector('.feed .on');
+  if (active) {
+    active.classList.remove('on');
+  }
+
+  if (!isShowing) {
+    el.classList.add('on');
+    el.classList.add('read');
+
+    // Insert content iframe if it's not already present.
+    if (!el.querySelector('iframe')) {
+      var table = el.parentNode.parentNode.dataset['table'];
+      var id = el.dataset['id'];
+
+      news.db.readTransaction(function(tx) {
+        tx.executeSql('SELECT content FROM ' + table + ' WHERE id=' + id, [],
+          function(tx, r) {
+            var iframe = [
+              '<iframe src="data:text/html,',
+              encodeURIComponent(r.rows.item(0).content), '"></iframe>'
+              ].join('');
+
+            el.querySelector('.snippet').insertAdjacentHTML('beforeEnd', iframe);
+
+          }, news.onError);
+      });
+    }
+  }
+
+}
+
+function onScroll(e) {
+  var overlays = document.querySelectorAll('.overlay');
+  for (var i = 0, overlay; overlay = overlays[i]; ++i) {
+    overlay.style.visibility = 'visible'; // Prevent flicker at page load.
+    overlay.classList.add('show');
+  }
+  window.removeEventListener('scroll', onScroll, false);
+}
+
+window.openBackgroundWindow = function() {
+  window.bgWindow = window.open(BG_WIN_URL, BG_WIN_NAME, 'background');
+};
+
+window.closeBackgroundWindow = function() {
+  var w = window.open(BG_WIN_URL, BG_WIN_NAME, 'background');
+  w.close();
+};
+
+window.toggleBgContentUpdating = function(el) {
+  if (el.checked) {
+    window.openBackgroundWindow();
+    $('#refresh-rate-container').show();
+  } else {
+    window.closeBackgroundWindow();
+    $('#refresh-rate-container').hide();
+  }
+  window.localStorage.setItem('updateSiteInBg', el.checked);
+};
+
+function toggleHelp() {
+  document.querySelector('.help').classList.toggle('hidden');
+  document.body.classList.toggle('dim');
+  window.focus();
+}
+
+function toggleFeedVisibility(id) {
+  document.querySelector('#' + id).classList.toggle('hidden');
+}
+
+window.addEventListener('scroll', onScroll, false);
+
+window.addEventListener('keydown', function(e) {
+
+  if ([27, 32, 72, 74, 75, 66, 84, 85].indexOf(e.keyCode) != -1) {
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    switch (e.keyCode) {
+      case 27: // ESC
+        // First hide help shadow box. Second esc hides any open articles.
+        document.querySelector('.help').classList.add('hidden');
+        if (document.body.classList.contains('dim')) {
+          document.body.classList.remove('dim');
+        } else {
+          $('.feed li.on').click();
+        }
+        return;
+        break;
+      case 32: // space
+        var li = $('.feed li:not(.read):first');
+        break;
+      case 72: // h
+        toggleHelp();
+        return;
+        break;
+      case 74: // j
+        var li = $('.feed li.on').next('li');
+        break;
+      case 75: // k
+        var li = $('.feed li.on').prev('li');
+        break;
+      case 66: // b
+        var li = $('.feed li:last');
+        break;
+      case 84: // t
+      case 85: // u
+        var li = $('.feed li:first');
+        break;
+    }
+
+    if (li.length && !document.body.classList.contains('dim')) {
+      li.click();
+      news.updateReadState(li.closest('.feed').data('table'), li.data('id'));
+    }
+  }
+
+}, false);
+
+// Refreshes the page if a new manifest is available. Handles the case where
+// user doesn't have app installed.
+window.applicationCache.addEventListener('updateready', function(e) {
+  if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+    if(confirm('A new version of this app is available. Load it?')) {
+      window.location.reload();
+    }
+  }
+}, false);
+
+document.querySelector('#feeds').addEventListener('webkitTransitionEnd', function(e) {
+  if (e.propertyName == 'height' && e.target.nodeName == 'LI' &&
+      e.target.classList.contains('on')) {
+    $.scrollTo($(e.target), 800, {offset: {top:-40, left:0}});
+    //currentArticle.table = $(e.target).closest('.feed').data('table');
+  }
+}, false);
+
+document.querySelector('#refresh-rate').addEventListener('change', function(e) {
+  this.value = this.value || 30;
+
+  var MIN_POLL = 3;
+  if (this.value < MIN_POLL) {
+    if (!confirm('Refreshing articles more than once every ' + MIN_POLL +
+                 ' minutes is not adviced.')) {
+      this.value = window.localStorage.getItem('refreshRate');
+      return;
+    }
+  }
+
+  window.localStorage.setItem('refreshRate', this.value);
+  if (window.bgWindow) {
+    window.bgWindow.setupPolling();
+  }
+}, false);
+
+
+(function() {
+// Check for support
+if (window.chrome && window.chrome.app && window.chrome.app.isInstalled) {
+  var CHROME_VERSION = parseFloat(navigator.userAgent.split('Chrome')[1].split(' ')[0].substring(1));
+  var MIN_CHROME_VER = 10;
+  if (CHROME_VERSION >= MIN_CHROME_VER) {
+    document.getElementById('supported').className = '';
+
+    var updateSiteInBg = JSON.parse(window.localStorage.getItem('updateSiteInBg'));
+    if (updateSiteInBg == undefined) {
+      updateSiteInBg = true;
+    }
+    document.getElementById('update-checkbox').checked = updateSiteInBg;
+    if (updateSiteInBg) {
+      window.openBackgroundWindow();
+      $('#refresh-rate-container').show();
+    }
+  }
+} else {
+  document.getElementById('unsupported').className = '';
+}
+
+news.init();
+initSettingsPanel();
+})();
---
+++ html5-demos.appspot.com/bgnews/js/news.js
@@ -0,0 +1,236 @@
+/*Copyright 2011 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author: Eric Bidelman (ericbidelman@chromium.org)
+*/
+
+window.news = window.news || {};
+
+news.FEEDS = {
+  'tc': 'http://feeds.feedburner.com/TechCrunch',
+  'cnn': 'http://rss.cnn.com/rss/cnn_topstories.rss',
+  'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+  'google': 'http://news.google.com/news?output=rss',
+  'nyt': 'http://feeds.nytimes.com/nyt/rss/World',
+  'yahoo': 'http://rss.news.yahoo.com/rss/yahoonewsroom',
+  'huffington': 'http://feeds.huffingtonpost.com/huffingtonpost/raw_feed',
+  //'abc': 'http://feeds.abcnews.com/abcnews/2020headlines',
+  'npr': 'http://www.npr.org/rss/rss.php?id=1001'
+};
+
+news.NUM_FEEDS = Object.keys(news.FEEDS).length;
+news.processedFeeds = 0;
+news.MAX_NUM_ARTICLES = 20; // Max number of articles to return from each feed.
+
+
+news.onError = function(tx, e) {
+  console.log('Something unexpected happened: ' + e.message);
+};
+
+news.onSuccess = function(tx, r) {
+  // NOOP
+};
+
+news.init = function() {
+  var dbSize = 5 * 1024 * 1024; // 5MB
+  news.db = window.openDatabase('News', '1.0', 'news articles', dbSize);
+  news.fetchNews();
+};
+
+news.reset = function() {
+  var d = document;
+  if (window.opener) {
+    d = window.opener.document;
+  }
+  var feeds = d.getElementById('feeds');
+  if (feeds) {
+    feeds.innerHTML = '';
+  }
+
+  var droppedTables = 0;
+
+  news.dropAll(function() {
+    droppedTables++;
+    if (droppedTables == news.NUM_FEEDS) {
+      news.fetchNews();
+    }
+  });
+};
+
+news.fetchNews = function() {
+  for (var table in news.FEEDS) {
+    news.createArticleTable(table);
+
+    (function(table) {
+      news.getAllNews(table, function(tx, result, table) {
+        if (result.rows.length) {  // Results in db. Render data from there.
+          news.renderNewsArticles(tx, result, table);
+        } else { // Fetch data from RSS feeds.
+
+          var script = document.createElement('script');
+          script.src = ['http://ajax.googleapis.com/ajax/services/feed/load',
+              '?v=1.0&num=', news.MAX_NUM_ARTICLES, '&q=', news.FEEDS[table],
+              '&callback=processFeedResponse&context=', table].join('');
+
+          // If script already existing on DOM (with same src), remove it.
+          var preExistingScript =
+              document.body.querySelector('script[src="' + script.src + '"]');
+          if (preExistingScript) {
+            document.body.removeChild(preExistingScript);
+          }
+
+          document.documentElement.lastChild.appendChild(script);
+        }
+      });
+    })(table);
+  }
+};
+
+news.createArticleTable = function(table) {
+  news.db.transaction(function(tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS ' + 
+        table + '(id INTEGER PRIMARY KEY ASC, ' +
+        'content TEXT, snippet TEXT, title STRING UNIQUE, link varchar(200), ' +
+        'publishedDate DATETIME, addedOn DATETIME, feed STRING, read BOOLEAN DEFAULT 0)',
+        []);
+  });
+};
+
+news.updateReadState = function(table, id, callback) {
+  news.db.transaction(function(tx) {
+    tx.executeSql('UPDATE ' + table + " SET read=1 WHERE id=" + id, [],
+                  null, news.onError);
+  });
+};
+
+news.dropAll = function(opt_callback) {
+  var callback = opt_callback || news.onSuccess;
+
+  news.db.transaction(function(tx) {
+    for (var key in news.FEEDS) {
+      tx.executeSql('DROP TABLE ' + key, [], callback, news.onError);
+    }
+  });
+};
+
+news.getAllNews = function(table, renderFunc) {
+  news.db.readTransaction(function(tx) {
+    tx.executeSql('SELECT * FROM ' + table, [], function(tx, result) {
+      renderFunc(tx, result, table);
+    }, news.onError);
+  });
+};
+
+news.articleHTML = function(entry) {
+  var date = new Date(entry.publishedDate);
+
+  var formattedDateStr = date.getMonth() + 1 + '/' + date.getDate() + ' ' +
+                         news.formatTimeString(date.toLocaleTimeString());
+
+  return ['<li onclick="onArticleOpen(this)" data-id="', entry.id,
+          '" class="', entry.read ? 'read' : '', '">',
+          '<h4><a href="', entry.link, '" target="_new">', entry.title,
+          '</a>', '<span class="date">', formattedDateStr, '</span></h4>',
+          '<div class="snippet"><span>', entry.snippet, '</span></div>',
+          '</li>'].join('');
+};
+
+news.formatTimeString = function(timeStr) {
+  var time = timeStr.split(':');
+
+  var d = new Date();
+  d.setHours(parseInt(time[0], 10), parseInt(time[1], 10));
+
+  var hours = d.getHours();
+  var minStr = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+  if (hours > 12) {
+    return [hours - 12, ':', minStr, ' PM'].join('');
+  } else {
+    var amPm = ' AM';
+    if (hours == 12) {
+      amPm = ' PM';
+    }
+    return [hours, ':', minStr, amPm].join('');
+  }
+};
+
+news.renderNewsArticles = function(tx, results, table) {
+  var d = document;
+  if (window.opener) {
+    d = window.opener.document;
+  }
+
+  var container = d.getElementById('feeds');
+
+  if (container) {
+    container.insertAdjacentHTML(
+        'beforeEnd', '<div class="feed" data-table="' + table + '"></div>');
+
+    container = container.querySelector("div[class='feed']:last-of-type");
+
+    if (!results.rows.length) {
+      container.insertAdjacentHTML(
+        'beforeEnd', '<li>No results.</li>');
+      return;
+    }
+
+    var html = [];
+    for (var i = 0; i < results.rows.length; ++i) {
+      html.push(news.articleHTML(results.rows.item(i)));
+    }
+
+    container.insertAdjacentHTML(
+        'beforeEnd', '<ul>' + html.join('') + '</ul>');
+
+    container.insertAdjacentHTML(
+        'afterBegin', '<h2>' + results.rows.item(0).feed + '</h2>');
+  }
+};
+
+news.addArticle = function(table, article, callback) {
+  news.db.transaction(function(tx) {
+
+    var pubDate = new Date(article.publishedDate);
+
+    tx.executeSql('INSERT INTO ' + table + '(content, snippet, title, link, ' +
+                  'publishedDate, addedOn, feed) VALUES (?,?,?,?,?,?,?)',
+                  [article.content, article.snippet, article.title,
+                  article.link, pubDate, new Date(), article.feed],
+                  callback, news.onError);
+  });
+};
+
+window.processFeedResponse = function(table, result, status, details, unused) {
+
+  news.processedFeeds++;
+
+  var callback = function(tx, r) {
+    if (news.processedFeeds == news.NUM_FEEDS) {
+      news.processedFeeds = 0;
+      news.fetchNews();
+    }
+  };
+
+  for (var i = 0, entry; entry = result.feed.entries[i]; ++i) {
+    news.addArticle(table, {
+      title: entry.title,
+      link: entry.link,
+      publishedDate: new Date(entry.publishedDate),
+      content: entry.content,
+      snippet: entry.contentSnippet,
+      feed: result.feed.title
+    }, callback);
+  }
+
+};
