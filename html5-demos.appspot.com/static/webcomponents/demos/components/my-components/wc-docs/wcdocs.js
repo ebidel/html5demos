7ef7d4a8b05a9d89adff624873990b50fb6ddeac
Eric Bidelman <ebidel@gmail.com>
2013-04-08 11:25:26 -0700

    GA

--- html5-demos.appspot.com/static/webcomponents/demos/components/my-components/wc-docs/wcdocs.js
+++ html5-demos.appspot.com/static/webcomponents/demos/components/my-components/wc-docs/wcdocs.js
@@ -18,4 +18,15 @@ if (!!!window.Toolkit) { //if (this === window) {
   // document.addEventListener('WebComponentsReady', function(e) {
   //   document.querySelector('wc-documentation').config(CONFIG);
   // });
+
+  // Only record for demo purposes.
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22014378-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 }
